b0d34ca13a3917f74272faad005a0627d6852ddb
OliverMineau <oliver.mineau42@orange.fr>
2022-12-02 14:36:23 +0100

    Trigger

--- data/v1_createTriggers.sql
+++ data/v1_createTriggers.sql
@@ -1,1 +1,8 @@
 -- TODO 3.3 Créer un trigger pertinent
+CREATE TRIGGER trigger_dateNais
+BEFORE INSERT ON LesSportifsEQ
+FOR EACH ROW
+WHEN (new.dateNaisSp > DATE())
+BEGIN
+    SELECT RAISE (ABORT, "Date de naissance incorrecte");
+END;
